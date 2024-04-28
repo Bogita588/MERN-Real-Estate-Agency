dd8e887c56e76489e87e043b517305a2a9adec6c
bogitabrian@gmail.com <bogitabrian@gmail.com>
2024-04-28 13:19:52 +0300

    Added filter components for property search

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,34 +1,35 @@
-import "./filter.scss";
+import "./customFilter.scss";
 
 function Filter() {
   return (
-    <div className="filter">
+    <div className="customFilter">
       <h1>
-        Search results for <b>London</b>
+        Search results for <b>Nairobi</b>
       </h1>
-      <div className="top">
-        <div className="item">
-          <label htmlFor="city">Location</label>
+      <div className="customTopSection">
+        <div className="customFormItem">
+          <label htmlFor="city" className="customLabel">Location</label>
           <input
             type="text"
             id="city"
             name="city"
+            className="customInput"
             placeholder="City Location"
           />
         </div>
       </div>
-      <div className="bottom">
-        <div className="item">
-          <label htmlFor="type">Type</label>
-          <select name="type" id="type">
+      <div className="customBottomSection">
+        <div className="customFormItem">
+          <label htmlFor="type" className="customLabel">Type</label>
+          <select name="type" id="type" className="customSelect">
             <option value="">any</option>
             <option value="buy">Buy</option>
             <option value="rent">Rent</option>
           </select>
         </div>
-        <div className="item">
-          <label htmlFor="property">Property</label>
-          <select name="property" id="property">
+        <div className="customFormItem">
+          <label htmlFor="property" className="customLabel">Property</label>
+          <select name="property" id="property" className="customSelect">
             <option value="">any</option>
             <option value="apartment">Apartment</option>
             <option value="house">House</option>
@@ -36,35 +37,38 @@ function Filter() {
             <option value="land">Land</option>
           </select>
         </div>
-        <div className="item">
-          <label htmlFor="minPrice">Min Price</label>
+        <div className="customFormItem">
+          <label htmlFor="minPrice" className="customLabel">Min Price</label>
           <input
             type="number"
             id="minPrice"
             name="minPrice"
+            className="customInput"
             placeholder="any"
           />
         </div>
-        <div className="item">
-          <label htmlFor="maxPrice">Max Price</label>
+        <div className="customFormItem">
+          <label htmlFor="maxPrice" className="customLabel">Max Price</label>
           <input
             type="text"
             id="maxPrice"
             name="maxPrice"
+            className="customInput"
             placeholder="any"
           />
         </div>
-        <div className="item">
-          <label htmlFor="bedroom">Bedroom</label>
+        <div className="customFormItem">
+          <label htmlFor="bedroom" className="customLabel">Bedroom</label>
           <input
             type="text"
             id="bedroom"
             name="bedroom"
+            className="customInput"
             placeholder="any"
           />
         </div>
-        <button>
-          <img src="/search.png" alt="" />
+        <button className="customSearchButton">
+          <img src="/search.png" alt="" className="customSearchIcon" />
         </button>
       </div>
     </div>
