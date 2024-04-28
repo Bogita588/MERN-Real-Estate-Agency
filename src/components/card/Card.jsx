c9620ad5618c084e8a911b9e57789a99eb2ea9f2
bogitabrian@gmail.com <bogitabrian@gmail.com>
2024-04-28 19:35:26 +0300

    Added Card component to display properties

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,37 +1,37 @@
 import { Link } from "react-router-dom";
 import "./card.scss";
 
-function Card({ item }) {
+function CustomCard({ item }) {
   return (
-    <div className="card">
-      <Link to={`/${item.id}`} className="imageContainer">
+    <div className="customCard">
+      <Link to={`/${item.id}`} className="customImageContainer">
         <img src={item.img} alt="" />
       </Link>
-      <div className="textContainer">
-        <h2 className="title">
+      <div className="customTextContainer">
+        <h2 className="customTitle">
           <Link to={`/${item.id}`}>{item.title}</Link>
         </h2>
-        <p className="address">
+        <p className="customAddress">
           <img src="/pin.png" alt="" />
           <span>{item.address}</span>
         </p>
-        <p className="price">$ {item.price}</p>
-        <div className="bottom">
-          <div className="features">
-            <div className="feature">
+        <p className="customPrice">$ {item.price}</p>
+        <div className="customBottom">
+          <div className="customFeatures">
+            <div className="customFeature">
               <img src="/bed.png" alt="" />
               <span>{item.bedroom} bedroom</span>
             </div>
-            <div className="feature">
+            <div className="customFeature">
               <img src="/bath.png" alt="" />
               <span>{item.bathroom} bathroom</span>
             </div>
           </div>
-          <div className="icons">
-            <div className="icon">
+          <div className="customIcons">
+            <div className="customIcon">
               <img src="/save.png" alt="" />
             </div>
-            <div className="icon">
+            <div className="customIcon">
               <img src="/chat.png" alt="" />
             </div>
           </div>
@@ -41,4 +41,4 @@ function Card({ item }) {
   );
 }
 
-export default Card;
+export default CustomCard;
